740532bba3963efe34ab41e5307344dd8ceb45e0
zhouzhongtao <zhouzhongtaosoft@163.com>
2018-05-19 22:15:12 +0800

    增加延时，伪造请求头，避免被封

--- fetcher/fetcher.go
+++ fetcher/fetcher.go
@@ -19,7 +19,10 @@ var rateLimit = time.Tick(1000 * time.Millisecond)
 // Fetch content from url
 func Fetch(url string) ([]byte, error) {
 	<-rateLimit
-	resp, err := http.Get(url)
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.108 Safari/537.36")
+	resp, err := client.Do(req)
 	if err != nil {
 		return nil, err
 	}
